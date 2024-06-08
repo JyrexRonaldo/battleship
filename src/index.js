caf61b012216827c5054795bb5eac793d7aee5b5
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2024-06-08 13:53:58 +0000

    Update gameboard factory

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // import "./reset.css";
 // import "./style.css";
 
-function ship(length) {
+function ship(length, name) {
   let hitCount = 0;
   const shipLength = length;
+  const shipName = name;
 
   const hit = () => {
     hitCount += 1;
@@ -11,26 +12,58 @@ function ship(length) {
 
   const isSunk = () => hitCount >= shipLength;
 
-  return { hit, isSunk };
+  const getShipLength = () => shipLength;
+
+  const getShipName = () => shipName;
+
+  return { hit, isSunk, getShipLength, getShipName };
 }
+
+const coordinates = (xPos, yPos) => {
+  const xCoordinate = xPos;
+  const yCoordinate = yPos;
+
+  const getXCoordinate = () => xCoordinate;
+  const getYCoordinate = () => yCoordinate;
+
+  let occupied = false;
+
+  let shipName = null;
+
+  let attacked = false;
+
+  const occupy = () => {
+    occupied = true;
+  };
+
+  const setShipName = (name) => {
+    shipName = name;
+  };
+
+  const getShipName = () => shipName;
+
+  const getOccupiedStatus = () => occupied;
+
+  const setAttacked = () => {
+    attacked = true;
+  };
+
+  const getAttackedStatus = () => attacked;
+
+  return {
+    getXCoordinate,
+    getYCoordinate,
+    occupy,
+    getOccupiedStatus,
+    setShipName,
+    getShipName,
+    setAttacked,
+    getAttackedStatus,
+  };
+};
 
 function gameboard() {
   const board = [];
-
-  const coordinates = (xPos, yPos) => {
-    const xCoordinate = xPos;
-    const yCoordinate = yPos;
-
-    let occupied = false;
-
-    const occupy = () => {
-      occupied = true;
-    };
-
-    const getOccupiedStatus = () => occupied;
-
-    return { xCoordinate, yCoordinate, occupy, getOccupiedStatus };
-  };
 
   for (let x = 0; x < 10; x += 1) {
     board[x] = [];
@@ -39,7 +72,85 @@ function gameboard() {
     }
   }
 
-  return {};
+  const ships = {
+    carrier: ship(5, "carrier"),
+    battleship: ship(4, "battleship"),
+    cruiser: ship(3, "cruiser"),
+    submarine: ship(3, "submarine"),
+    destroyer: ship(2, "destroyer"),
+  };
+
+  const availableCoordinates = (length, xPos, yPos, direction) => {
+    // true means horizontal direction
+    // false means vertical direction
+    let available = null;
+    if (direction) {
+      for (let i = 0; i < length; i += 1) {
+        available = board[xPos + i][yPos].occupied === false;
+        if (available === false) {
+          break;
+        }
+      }
+    } else {
+      for (let i = 0; i < length; i += 1) {
+        available = board[xPos][yPos + i].occupied === false;
+        if (available === false) {
+          break;
+        }
+      }
+    }
+
+    return available;
+  };
+
+  const placeShip = (shipType, xPos, yPos, direction) => {
+    const length = shipType.getShipLength;
+    const freeSpace = availableCoordinates(length, xPos, yPos, direction);
+    let message = null;
+
+    if (freeSpace) {
+      // true means horizontal direction
+      // false means vertical direction
+      if (direction) {
+        for (let i = 0; i < length; i += 1) {
+          board[xPos + i][yPos].occupy();
+        }
+      } else {
+        for (let i = 0; i < length; i += 1) {
+          board[xPos][yPos + i].occupy();
+        }
+      }
+    } else {
+      message = "can't place ship";
+    }
+
+    return message;
+  };
+
+  const receiveAttack = (xPos, yPos) => {
+    const attackCoordinate = board[xPos][yPos];
+    attackCoordinate.setAttacked();
+    if (attackCoordinate.getOccupiedStatus()) {
+      const attackedShip = attackCoordinate.getShipName();
+      ships[attackedShip].hit();
+    }
+  };
+
+  return { placeShip, receiveAttack };
 }
 
 export { ship, gameboard };
+
+// const ships = {
+//   carrier: ship(5),
+//   battleship: ship(4),
+//   cruiser: ship(3),
+//   submarine: ship(3),
+//   destroyer: ship(2)
+// };
+
+// const placeShip = (shipType, xPos, yPos) => {
+//     let coordinate = board[xPos][yPos]
+
+//     coordinate.occupy()
+//   }
